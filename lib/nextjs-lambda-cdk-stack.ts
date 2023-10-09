6dbb5337c723a6900647def89a90d17f77d20926
ivan-arteaga-tfgm <ivan.arteaga@tfgm.com>
2023-10-09 16:14:55 +0000

    update

--- lib/nextjs-lambda-cdk-stack.ts
+++ lib/nextjs-lambda-cdk-stack.ts
@@ -3,6 +3,7 @@ import { Construct } from 'constructs';
 import * as cdk from 'aws-cdk-lib';
 
 import path from 'path';
+import { AdotLambdaLayerPythonSdkVersion } from 'aws-cdk-lib/aws-lambda';
 
 export class NextjsLambdaCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -26,6 +27,27 @@ export class NextjsLambdaCdkStack extends cdk.Stack {
         compatibleArchitectures: [
           cdk.aws_lambda.Architecture.X86_64,
           cdk.aws_lambda.Architecture.ARM_64,
+        ],
+        compatibleRuntimes: [
+          cdk.aws_lambda.Runtime.NODEJS_18_X,
+          cdk.aws_lambda.Runtime.NODEJS_16_X,
+          cdk.aws_lambda.Runtime.NODEJS_14_X,
+          cdk.aws_lambda.Runtime.PYTHON_3_11,
+          cdk.aws_lambda.Runtime.PYTHON_3_9,
+          cdk.aws_lambda.Runtime.PYTHON_3_8,
+          cdk.aws_lambda.Runtime.PYTHON_3_7,
+          cdk.aws_lambda.Runtime.PYTHON_3_6,
+          cdk.aws_lambda.Runtime.RUBY_2_7,
+          cdk.aws_lambda.Runtime.RUBY_3_2,
+          cdk.aws_lambda.Runtime.JAVA_11,
+          cdk.aws_lambda.Runtime.JAVA_17,
+          cdk.aws_lambda.Runtime.JAVA_8_CORRETTO,
+          cdk.aws_lambda.Runtime.JAVA_8,
+          cdk.aws_lambda.Runtime.GO_1_X,
+          cdk.aws_lambda.Runtime.DOTNET_6,
+          cdk.aws_lambda.Runtime.DOTNET_CORE_3_1,
+          cdk.aws_lambda.Runtime.PROVIDED,
+          cdk.aws_lambda.Runtime.PROVIDED_AL2,
         ],
       }
     );
